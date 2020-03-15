8aa17cbdc7381e9d988759a184177f667bb89744
“dinadeljanin” <ddeljanin5@icloud.com>
2020-03-15 01:20:03 -0400

    initial commit for ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
+import NavBar from './NavBar';
 import '../css/App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
