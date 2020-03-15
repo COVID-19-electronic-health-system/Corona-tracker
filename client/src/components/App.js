12481b574eadedf848a548be9f09ee0455f65ab3
Carter Klein <carterklein13@gmail.com>
2020-03-15 13:05:28 -0400

    Incorporate Blockstack authentication to begin fleshing out serverless backend and database

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBar from './NavBar';
+import Login from './Login';
 import '../css/App.css';
+import { appConfig } from '../utils/constants'
+import { UserSession } from 'blockstack';
 
+const userSession = new UserSession({ appConfig: appConfig })
 
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+  componentDidMount() {
+    if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then((userData) => {
+        window.history.replaceState({}, document.title, "/")
+        this.setState({ userData: userData })
+      });
+    }
+  }
+
+  handleSignIn(e) {
+    e.preventDefault()
+    userSession.redirectToSignIn()
+  }
+
+  handleSignOut(e) {
+    e.preventDefault();
+    userSession.signUserOut(window.location.origin);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {!userSession.isUserSignedIn() ?
+          <Login handleSignIn={this.handleSignIn} />
+          : <div>
+            <NavBar />
+            <button onClick={this.handleSignOut}>Sign Out</button>
+          </div>
+        }
+      </div>
+    );
+  }
 }
-
-export default App;
