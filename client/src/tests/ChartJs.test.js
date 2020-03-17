a692f3137fcdfe6045ea35cac331b91e1f9d2c9a
akilhylton <hyltonakil@gmail.com>
2020-03-17 18:02:23 -0400

    Fixed broken import

--- client/src/tests/ChartJs.test.js
+++ client/src/tests/ChartJs.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import TempChart from '../components/Chart';
+import TempChart from '../components/ChartJs';
 
 test('renders sub component TempChart', () => {
   render(<TempChart />);
