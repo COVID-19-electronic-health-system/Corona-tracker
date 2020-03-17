f5e51cd0d4c03ee0ca70c0ad80259be94182e779
Akil M Hylton <hyltonakil@gmail.com>
2020-03-17 18:09:45 -0400

    Update ChartJs.test.js

--- client/src/tests/ChartJs.test.js
+++ client/src/tests/ChartJs.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import TempChart from '../components/ChartJs';
+import ChartJs from '../components/ChartJs';
 
 test('renders sub component TempChart', () => {
-  render(<TempChart />);
+  render(<ChartJs />);
 });
