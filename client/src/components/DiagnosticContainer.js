e0d50240489016cb917b722375078743c919a6d1
Friedger Müffke <mail@friedger.de>
2020-03-22 17:46:33 +0100

    remove unused data

--- client/src/components/DiagnosticContainer.js
+++ client/src/components/DiagnosticContainer.js
@@ -22,21 +22,23 @@ function DiagnosticContainer(props) {
   return (
     <div className="DiagnosticContainer">
       <NavBar />
-      <Container className='temp-singout'>
-      <Temperature allRecords={[{ temperature: 100.4 }, { temperature: 98 }]} />
-      <Button onClick={handleSignOut} style={{width: '100px'}}>Sign Out</Button>
+      <Container className="temp-singout">
+        <Temperature />
+        <Button onClick={handleSignOut} style={{ width: '100px' }}>
+          Sign Out
+        </Button>
       </Container>
-      
+
       <Container>
-      <Logo className="DiagnosticLogo" />
-      <TextLogo className="DiagnosticTextLogo" />
-      <h4>
-      Welcome!, <b>{userSession.loadUserData().profile.name}</b>
-      </h4>
-      <h5>
-      Today is <b>{today.toLocaleDateString(undefined, dateOptions)}</b>{" "}
-      </h5>
-      <hr className="hr" />
+        <Logo className="DiagnosticLogo" />
+        <TextLogo className="DiagnosticTextLogo" />
+        <h4>
+          Welcome!, <b>{userSession.loadUserData().profile.name}</b>
+        </h4>
+        <h5>
+          Today is <b>{today.toLocaleDateString(undefined, dateOptions)}</b>{' '}
+        </h5>
+        <hr className="hr" />
       </Container>
     </div>
   );
