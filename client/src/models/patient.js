9cf2b58a32b3c048accd59220e76c509d5d4c787
Carter Klein <carterklein13@gmail.com>
2020-03-22 11:43:36 -0400

    Added checkup and initial evaluation models (#114)

--- client/src/models/patient.js
+++
@@ -1,9 +0,0 @@
-import { Model } from 'radiks';
-
-export default class Patient extends Model {
-    static className = 'Patient';
-    static schema = { // all fields are encrypted by default
-        doctor: String,
-        location: Array,
-    }
-};
--- design/database_models/README.md
+++
@@ -1,6 +0,0 @@
-# Database Design
-
-## Design for the Health Log 
-As of 3/14/20, 4:00PM: 
-
-![Current Design](images/health-log-03-14-20.png?raw=true "Current Design")
