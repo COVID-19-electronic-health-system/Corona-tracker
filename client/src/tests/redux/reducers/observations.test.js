b821755f864d45af2bf30100a57f26486d97ae1a
Friedger Müffke <mail@friedger.de>
2020-03-22 23:27:59 +0100

    add test for observation reducer

---
+++ client/src/tests/redux/reducers/observations.test.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+import reducer from '../../../redux/reducers/observations'
+import { OBSERVATIONS_LOADED } from '../../../redux/actions/actions';
+
+describe('obervations reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toHaveLength(4);
+  });
+
+  it('should handle OBSERVATIONS_LOADED', () => {
+    const loadedAction = {
+      type: OBSERVATIONS_LOADED,
+      observations = [new Observation({physical:{temperature:90}})]
+    };
+    // it's empty on purpose because it's just starting to fetch posts
+    expect(reducer({}, loadedAction)).toEqual(loadedAction.observations);
+  }
+  )
+
+   });
+
