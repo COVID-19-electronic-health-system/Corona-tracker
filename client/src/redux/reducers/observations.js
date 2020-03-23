97be677686bb0ac7c564eb6d75ca9e43ec201dbd
Friedger Müffke <mail@friedger.de>
2020-03-23 01:52:43 +0100

    Use reducer for observations (#145)

---
+++ client/src/redux/reducers/observations.js
@@ -0,0 +1,22 @@
+import Observation from '../../models/observation';
+import { OBSERVATIONS_LOADED } from '../actions/actions';
+
+const initialState = [
+  new Observation({ physical: { temperature: 80 } }),
+  new Observation({ physical: { temperature: 70 } }),
+  new Observation({ physical: { temperature: 56 } }),
+  new Observation({ physical: { temperature: 90 } }),
+  new Observation({ physical: { temperature: 55 } }),
+];
+
+// Updating store based on type of the action
+const observationsReducer = (oldState = initialState, action) => {
+  switch (action.type) {
+    case OBSERVATIONS_LOADED:
+      return action.observations;
+    default:
+      return oldState;
+  }
+};
+
+export default observationsReducer;
