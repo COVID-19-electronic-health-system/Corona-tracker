6ccb638088d5c3a6ee2caabe0e53851277c6c9b0
Vesna Tan <tanvezi@gmail.com>
2020-03-23 02:56:07 -0400

    Add working Cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,47 +1,36 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { animated, interpolate } from "react-spring";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  // bullet: {
-  //   display: 'inline-block',
-  //   margin: '0 2px',
-  //   transform: 'scale(0.8)',
-  // },
-  // title: {
-  //   fontSize: 14,
-  // },
-  // pos: {
-  //   marginBottom: 12,
-  // },
-});
+class Card extends React.Component {
+  render() {
+    const { i, x, y, rot, scale, trans, bind, data } = this.props;
+    const { title, body, img } = data;
 
-const SimpleCard = (props) => {
-  const classes = useStyles();
-  const {title, body} = props;
-  console.log(title, body, props)
-  return (
-    <Card >
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {title}
-        </Typography>
-        <Typography variant="body2" component="p">
-         {body}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+      <animated.div
+        key={i}
+        style={{
+          transform: interpolate(
+            [x, y],
+            (x, y) => `translate3d(${x}px,${y}px,0)`
+          )
+        }}
+      >
+        <animated.div
+          {...bind(i)}
+          style={{
+            transform: interpolate([rot, scale], trans)
+          }}
+        >
+          <div className="card">
+            <h1>{title}</h1>
+            <h3>{body}</h3>
+          </div>
+        </animated.div>
+      </animated.div>
+    );
+  }
 }
 
-export default SimpleCard
+
+export default Card;
--- client/src/components/FlashCards.js
+++ client/src/components/FlashCards.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useSprings, animated, interpolate } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
-import SimpleCard from './Card'
+import Card from './Card'
 import '../css/FlashCards.css'
 
 const cards = [
@@ -13,31 +13,63 @@ const cards = [
   'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg'
 ]
 
-const AnimatedCard = animated(SimpleCard)
-
 const fakeCardData = [
   {title:'Did you know?', body:"In Covid-19,'CO' stands for 'corona', 'VI' for 'virus', and 'D' for disease. Formerly, this disease was referred to as 2019 novel coronavirus or 2019-nCov. There are many types of human coronaviruses including some that commonly cause mild upper-respiratory tract illness"},
-  {title:'1', body:'2'},
-  {title:'1', body:'6'},
-  {title:'1', body:'5'},
-  {title:'1', body:'4'},
-  {title:'1', body:'3'}
+  {
+    title:'Did you know?',
+    body:'This virus was first detected in Wuhan City, Hubei Province, China. The first infections were linked to a live animal market, but the virus is now spreading from person-to-person.',
+    img:'https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg'
+  },
+  {
+    title:'Did you know?',
+    body:'The virus causing coronavirus disease 2019 (COVID-19), is not the same as the coronaviruses that commonly circulate among humans and cause mild illness, like the common cold.',
+    img:'https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg'
+  },
+  {
+    title:'1',
+    body:'5',
+    img:'https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg'
+  },
+  {
+    title:'1',
+    body:'4',
+    img:'https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg'
+  },
+  {
+    title:'1',
+    body:'3',
+    img:'https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg'
+  }
 ]
 
-// These two are just helpers, they curate spring data, values that are later being interpolated into css
-const to = i => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
+const to = i => ({
+  x: 0,
+  y: i * -4,
+  scale: 1,
+  rot: -10 + Math.random() * 20,
+  delay: i * 100 
+})
 const from = i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
-// This is being used down there in the view, it interpolates rotation and scale into a css transform
+
 const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
 const FlashCards = () => {
-  const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
-  const [props, set] = useSprings(cards.length, i => ({ ...to(i), from: from(i) })) // Create a bunch of springs using the helpers above
-  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-  const bind = useDrag(({ args: [index], down, movement: [mx], distance, direction: [xDir], velocity }) => {
-    const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
-    const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
-    if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+  const [gone] = useState(() => new Set()) 
+  const [props, set] = useSprings(cards.length, i => ({ ...to(i), from: from(i) }))
+  const bind = useDrag(
+    ({
+      args: [index],
+      down,
+      movement: [mx],
+      distance,
+      direction: [xDir],
+      velocity 
+    }) => {
+    const trigger = velocity > 0.1
+    const dir = xDir < 0 ? -1 : 1
+    
+    if (!down && trigger) gone.add(index)
+
     set(i => {
       if (index !== i) return // We're only interested in changing spring-data for the current spring
       const isGone = gone.has(index)
@@ -50,10 +82,16 @@ const FlashCards = () => {
   })
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
   return props.map(({ x, y, rot, scale }, i) => (
-    <animated.div key={i} style={{ x, y }}>
-      {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
-      <AnimatedCard className="Card" fakecarddata={fakeCardData[i]} {...bind(i)} style={{ transform: interpolate([rot, scale], trans),  }} />
-    </animated.div>
+    <Card
+      i={i}
+      x={x}
+      y={y}
+      rot={rot}
+      scale={scale}
+      trans={trans}
+      data={fakeCardData[i]}
+      bind={bind}
+    />
   ))
 }
 
