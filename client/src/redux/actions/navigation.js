501e1b0120c1652f6979e5d6217b3f64d8039b4f
Yoni Sisso <yonis9@walla.com>
2020-03-24 16:24:33 +0200

    removed console.log

--- client/src/redux/actions/navigation.js
+++ client/src/redux/actions/navigation.js
@@ -1,9 +1,6 @@
 import { SET_NAVBAR_SECTION } from './actions';
 
-export const setNavbarSection = section => {
-console.log('avtion', section)
-
-return ({
+export const setNavbarSection = section => ({
     type: SET_NAVBAR_SECTION,
     payload: section
-})}
+})
