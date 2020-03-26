e503b8156827a9c86dcc126eaa8ed7fc99e05caf
AdhamAH <adham.ahasson@gmail.com>
2020-03-26 23:54:23 +0100

    changed FactQuiz style to go with layout

--- client/src/components/FactQuizContainer.js
+++ client/src/components/FactQuizContainer.js
@@ -2,33 +2,18 @@ import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { Button, ButtonGroup } from '@material-ui/core'
 import FlashCards from './FlashCards'
-import NavBar from './NavBar'
-import Temperature from './Temperature'
-import { ReactComponent as Logo } from '../img/Logo_CORONATRACKER_Logo.svg';
-import { ReactComponent as TextLogo } from '../img/Logo_CORONATRACKER_Text_Logo.svg';
 import { cardData, quizData } from '../utils/fakeCardData'
 import '../css/DiagnosticContainer.css';
 
 
 const FactQuizContainer = (props) => {
-  const { handleSignOut } = props
   const [factsOrQuiz, setFactsQuiz] = useState('facts')
   const setQuiz = () => setFactsQuiz("quiz")
   const setFacts = () => setFactsQuiz("facts")
 
   return (
-    <div className="DiagnosticContainer">
-      <Container className="temp-singout">
-        <Temperature />
-        <Button onClick={handleSignOut} style={{ width: '100px' }}>
-          Sign Out
-        </Button>
-      </Container>
+    <div>
       <Container >
-        <div className="Education">
-          <Logo className="DiagnosticLogo" style={{width:"2rem", height:"2rem"}}/>
-          <TextLogo className="DiagnosticTextLogo" style={{width:"15rem"}}/>
-        </div>
         <ButtonGroup size="medium" color="secondary" aria-label="outlined button group" style={{padding:'10px'}}>
           <Button onClick={setFacts} variant={factsOrQuiz==='facts'&&"contained"}>Facts</Button>
           <Button onClick={setQuiz} variant={factsOrQuiz==='quiz'&&"contained"}>Quiz</Button>
@@ -40,7 +25,6 @@ const FactQuizContainer = (props) => {
       </Container>
       {factsOrQuiz==='facts'&&<FlashCards mode='facts' cardData={cardData}/>}
       {factsOrQuiz==='quiz'&&<FlashCards mode='quiz' cardData={quizData}/>}
-      <NavBar />
     </div>
   )
 }
