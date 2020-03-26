ddd763acaea433c47d1ff9d41133e0ddc4950f43
Ariana Hwang <ajhwang0717@gmail.com>
2020-03-26 15:23:13 -0400

    fix: test passing

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -3,6 +3,11 @@ import { render } from '@testing-library/react';
 import App from '../components/App';
 import Map from '../components/Map';
 
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  Map: () => ({})
+}));
+
 test('renders App', () => {
   // render(<App />); TODO update this later
 });
+
