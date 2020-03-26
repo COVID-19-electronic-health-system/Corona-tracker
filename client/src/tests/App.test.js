3816b4c08e5ec5398f44158961550a9f8d3546b3
Ariana Hwang <ajhwang0717@gmail.com>
2020-03-26 17:08:37 -0400

    travis testing 3

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../components/App';
-import mapboxgl from "mapbox-gl/dist/mapbox-gl";
+// import mapboxgl from "mapbox-gl/dist/mapbox-gl";
 import Map from '../components/Map';
+
+
+if (typeof window.URL.createObjectURL === 'undefined') {
+  window.URL.createObjectURL = () => {
+    // Do nothing
+    // Mock this function for mapbox-gl to work
+  };
+}
 
 jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
   Map: () => ({})
 }));
+
 
 test('renders App', () => {
   // render(<App />); TODO update this later
