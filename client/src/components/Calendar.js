3aef2a4ca21e6478e0cd824514f9e0faac450992
JWaltuch <jenna.waltuch@gmail.com>
2020-03-27 13:47:03 -0400

    feat: calendar takes user to survey when clicking todays date

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,49 +1,50 @@
 import React, { createRef, useState } from 'react';
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import "@fullcalendar/core/main.css";
-import "@fullcalendar/daygrid/main.css";
-import "@fullcalendar/timegrid/main.css";
-import "../css/Calendar.css";
+import { useHistory } from 'react-router-dom';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+import '../css/Calendar.css';
 
-
-const AppCalendar = () => { 
-
+const AppCalendar = () => {
   // state emulation, store all events user created
-  const [calendarEvents, setCalendarEvents] = useState([])
+  const [calendarEvents, setCalendarEvents] = useState([]);
+
+  const history = useHistory();
 
   // create new event function
-  const handleCreateEventClick = (arg) => {
-    let arrayObject = {
-      title: 'new event',
-      start: arg.date,
-      allDay: arg.allDay
+  const handleCreateEventClick = args => {
+    let todaysDate = new Date().toISOString().slice(0, 10);
+    if (args.dateStr === todaysDate) {
+      history.push('/symptomsurvey');
     }
-    setCalendarEvents([...calendarEvents, arrayObject])
-  }
+  };
 
-  const calendarComponentRef = createRef()
-  
+  const calendarComponentRef = createRef();
+
   return (
-      <div>
-        <div className="app-calendar">
-          <FullCalendar
-            defaultView="dayGridMonth"
-            header={{
-              left: "prev,next today",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
-            }}
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            ref={calendarComponentRef}
-            events={calendarEvents}
-            dateClick={handleCreateEventClick}
-          />
-        </div>
+    <div>
+      <div className="app-calendar">
+        <FullCalendar
+          defaultView="dayGridMonth"
+          header={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+          }}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          ref={calendarComponentRef}
+          events={calendarEvents}
+          dateClick={args => {
+            handleCreateEventClick(args);
+          }}
+        />
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default AppCalendar;
