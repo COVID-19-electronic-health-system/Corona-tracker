0f078e7f4fdf8f268f4b401ec7cee6a7c23d082f
Enya <enya.baez@gmail.com>
2020-03-26 22:20:46 -0400

    fully connected to redux store

--- client/src/components/Disclaimer.js
+++ client/src/components/Disclaimer.js
@@ -4,33 +4,46 @@ import { ReactComponent as TextLogo } from '../img/Logo_CORONATRACKER_Text_Logo.
 import Button from 'react-bootstrap/Button';
 import { connect } from 'react-redux';
 import actions from '../redux/actions/actions';
+import { getConfig } from 'radiks';
 
 export const Disclaimer = props => {
   console.log(props);
 
+  const onClickDisagree = answer => {
+    props.setAnswer(answer);
+    //currently this will only update store with answer, still need to make it do whatever we decide should happen on disagree, such as log out
+  };
+
   return (
-    <div className="DisclaimerPopUp">
-      <div className="DisclaimerPopUpInner">
-        <Logo className="LoginLogo" className="PopUpLogo" />
-        <TextLogo className="TextLogo" className="PopUpLogo" />
-        <p>
-          The CoronaTracker is designed to help you navigate through the pandemic with accurate information, charting
-          your wellbeing, and tracking your health. The CoronaTracker is not intended to be used or viewed as diagnosis
-          or treatment of disease or other symptoms, including but not limited to COVID-19.
-        </p>
-        <p>
-          This application was made by the community for the community. This application is made for you! You are in
-          control of how you would like to contribute your data for public health and research.
-        </p>
-        <div>
-          <Button className="theme-Palette-white-text" onClick={() => props.setAnswer('agree')}>
-            I agree
-          </Button>
-          <Button className="theme-Palette-white-text theme-Palette-red-bg" onClick={() => props.setAnswer('disagree')}>
-            I don't agree
-          </Button>
+    <div>
+      {props.answer === null || props.answer === 'disagree' ? (
+        <div className="DisclaimerPopUp">
+          <div className="DisclaimerPopUpInner">
+            <Logo className="LoginLogo" className="PopUpLogo" />
+            <TextLogo className="TextLogo" className="PopUpLogo" />
+            <p>
+              The CoronaTracker is designed to help you navigate through the pandemic with accurate information,
+              charting your wellbeing, and tracking your health. The CoronaTracker is not intended to be used or viewed
+              as diagnosis or treatment of disease or other symptoms, including but not limited to COVID-19.
+            </p>
+            <p>
+              This application was made by the community for the community. This application is made for you! You are in
+              control of how you would like to contribute your data for public health and research.
+            </p>
+            <div>
+              <Button className="theme-Palette-white-text" onClick={() => props.setAnswer('agree')}>
+                I agree
+              </Button>
+              <Button
+                className="theme-Palette-white-text theme-Palette-red-bg"
+                onClick={() => onClickDisagree('disagree')}
+              >
+                I don't agree
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
