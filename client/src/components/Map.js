9c562d97d6103ee1ea6f777356c1df6fd49effae
Ariana Hwang <ajhwang0717@gmail.com>
2020-03-28 11:02:17 -0400

    fixed: deleted package.json and map.css, navbar in map.js, used material ui in map.js

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from "mapbox-gl";
-import NavBar from './NavBar';
-import '../css/Map.css'
+import { makeStyles } from '@material-ui/core/styles';
 
 const mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN
 
 
-const styles = {
-    width: "100vw",
-    height: "calc(100vh - 80px)",
-    position: "absolute"
-};
+const useStyles = makeStyles({
+    root: {
+        width: "100vw",
+        height: "calc(100vh - 80px)",
+        position: "absolute"
+    },
+    mapContainer: {
+        paddingTop: "3vh",
+        textAlign: "center",
+        height: "100vh"
+    }
+
+});
 
 
 function Map() {
+
+    const classes = useStyles();
 
     const [map, setMap] = useState(null);
     const mapContainer = useRef(null);
@@ -41,11 +50,10 @@ function Map() {
 
 
     return (
-        <div className="mapContainer">
+        <div className={classes.mapContainer}>
             <h4>Check out the Map!</h4>
             <h4>...COMING SOON...</h4>
-            <div ref={el => (mapContainer.current = el)} style={styles} />
-            <NavBar />
+            <div ref={el => (mapContainer.current = el)} className={classes.root} />
         </div>
 
     )
