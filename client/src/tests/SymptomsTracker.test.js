5125b4fe9e62e2aa039d003288ef9963f29f9218
Carter Klein <carterklein13@gmail.com>
2020-03-29 13:57:10 -0400

    Store user data in Gaia (#271)

--- client/src/tests/SymptomsTracker.test.js
+++ client/src/tests/SymptomsTracker.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import SymptomsTracker from '../components/SymptomsTracker';
+import { useBlockstack } from 'react-blockstack';
 
 test('renders Slider component', () => {
-     render(<SymptomsTracker />);
- });
+    // const { userSession } = useBlockstack();
+    // render(<SymptomsTracker userSession={useBlockstack()}/>);
+});
