a626f64fd44249b090fffd17239d8756c73ae7a4
crispinamuriel <crispinamuriel@gmail.com>
2020-03-29 04:31:14 -0400

    merge

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 // not like to use this?
-// have a look at the Quick start guide 
+// have a look at the Quick start guide
 // for passing in lng and translations on init
 
 i18n
