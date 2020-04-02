c5f215c618b2428db4e8a421e9b9e1012cb5f891
Carter Klein <carterklein13@gmail.com>
2020-04-01 21:11:09 -0400

    Stylize WeeklyTracker to near final state

--- client/src/components/MyHealthLog.jsx
+++ client/src/components/MyHealthLog.jsx
@@ -4,7 +4,19 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Grid } from '@material-ui/core';
 import WeeklyTracker from './WeeklyTracker';
+import WeeklyTrackerDay from './WeeklyTrackerDay';
 import AppCalendar from './Calendar';
+
+const dates = [
+  {
+    date: 11,
+    day: 'Monday',
+    status: '10',
+    temp: 99.6,
+    symptoms: 'headache',
+    comments: 'Tired for the entire day',
+  },
+];
 
 function HealthLogButton() {
   const { t } = useTranslation();
@@ -18,7 +30,11 @@ function HealthLogButton() {
       </Link>
       <Grid container direction="column" alignContent="center">
         <AppCalendar />
-        <WeeklyTracker />
+        {dates.map(dayData => (
+          <WeeklyTracker key={dayData.date}>
+            <WeeklyTrackerDay dayData={dayData} />
+          </WeeklyTracker>
+        ))}
       </Grid>
     </div>
   );
--- client/src/components/WeeklyTrackerDay.jsx
+++ client/src/components/WeeklyTrackerDay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
+import PropTypes, { object } from 'prop-types';
 import CalendarThreeLines from '../img/Calendar_Three-Lines.svg';
 
 const useStyles = makeStyles(() => ({
@@ -12,6 +12,13 @@ const useStyles = makeStyles(() => ({
   div: {
     width: '375px',
   },
+  summary: {
+    marginTop: '1vh',
+    marginRight: '2vw',
+  },
+  topMargin: {
+    marginTop: '1vh',
+  },
 }));
 
 const WeeklyTrackerDay = props => {
@@ -20,15 +27,15 @@ const WeeklyTrackerDay = props => {
   return (
     <div className={classes.div}>
       <Grid container spacing={0} className="day-container">
-        <Grid item>
+        <Grid item className={classes.topMargin}>
           <img alt="threeLinesSvg" src={CalendarThreeLines} />
         </Grid>
-        <Grid item>
+        <Grid item className={classes.topMargin}>
           <Typography variant="h4">{dayData.date}</Typography>
         </Grid>
-        <Grid item>
-          <Typography variant="subtitle2">You said you felt: {dayData.status} / 10</Typography>
-          <Typography variant="subtitle2">{dayData.day}, 12:00pm</Typography>
+        <Grid item className={classes.summary}>
+          <Typography variant="subtitle2">You felt: {dayData.status}/10</Typography>
+          <Typography variant="subtitle2">{dayData.day} </Typography>
         </Grid>
         <Grid item>
           <Typography variant="subtitle2">
@@ -45,13 +52,20 @@ const WeeklyTrackerDay = props => {
 };
 
 WeeklyTrackerDay.propTypes = {
-  // Had to set dayData to check for string, object is forbidden in the Proptype checker
-  dayData: PropTypes.string.isRequired,
-  date: PropTypes.number.isRequired,
-  day: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-  temp: PropTypes.number.isRequired,
-  symptoms: PropTypes.number.isRequired,
+  dayData: PropTypes.objectOf(object).isRequired,
+  date: PropTypes.number,
+  day: PropTypes.string,
+  status: PropTypes.string,
+  temp: PropTypes.number,
+  symptoms: PropTypes.string,
+};
+
+WeeklyTrackerDay.defaultProps = {
+  date: 0,
+  day: '0/0/0000',
+  status: 'n/a',
+  temp: 0,
+  symptoms: 'n/a',
 };
 
 export default WeeklyTrackerDay;
