cf55a2d82bf9d00ae2f8b466db7decf5d5c01e5d
pavel ilin <refugeebase@gmail.com>
2020-04-02 14:30:45 -0400

    add indication of the todays date on the Calendar

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Calendar from 'react-calendar';
@@ -13,13 +13,16 @@ const useStyles = makeStyles({
     maxWidth: '50%',
     maxheight: '50%',
   },
+  today: {
+    color: 'white',
+    backgroundColor: '#f64141',
+  },
 });
 
 const AppCalendar = () => {
   const classes = useStyles();
-
   const dispatch = useDispatch();
-
+  const [today] = useState(new Date().toISOString().slice(0, 10));
   const history = useHistory();
 
   // select date function
@@ -37,7 +40,15 @@ const AppCalendar = () => {
 
   return (
     <div className={classes.appCalendar}>
-      <Calendar onChange={handleDateClick} />
+      <Calendar
+        onChange={handleDateClick}
+        tileClassName={({ date, view }) => {
+          if (date.toISOString().slice(0, 10) === today) {
+            return classes.today;
+          }
+          return null
+        }}
+      />
     </div>
   );
 };
