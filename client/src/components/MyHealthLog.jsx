54e649929b1ea8356b094def38d4160ed082a139
Adham Abo Hasson <55054963+AdhamAH@users.noreply.github.com>
2020-04-03 22:47:52 +0200

    Take survey button (#367)

--- client/src/components/MyHealthLog.jsx
+++ client/src/components/MyHealthLog.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { Grid } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import WeeklyTracker from './WeeklyTracker';
 import WeeklyTrackerDay from './WeeklyTrackerDay';
 import AppCalendar from './Calendar';
+
+const useStyles = makeStyles({
+  button: {
+    background: `linear-gradient(45deg, #4760ff, #82a4f8)`,
+    width: '320px',
+    boxShadow: '0px 1px 15px 0px #4760ff',
+    margin: '10px',
+    color: 'white',
+  },
+});
 
 const dates = [
   {
@@ -20,11 +30,11 @@ const dates = [
 
 function HealthLogButton() {
   const { t } = useTranslation();
-
+  const classes = useStyles();
   return (
     <div>
       <Link to="/symptomsurvey">
-        <Button color="secondary" variant="contained">
+        <Button className={classes.button} variant="contained">
           {t('surveyButtonText')}
         </Button>
       </Link>
