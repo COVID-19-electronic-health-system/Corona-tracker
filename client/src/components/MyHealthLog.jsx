51e0907f94d50c4301294a6d06394ffde7dd8eb1
AdhamAH <adham.ahasson@gmail.com>
2020-04-03 22:31:31 +0200

    restyle-take-survey

--- client/src/components/MyHealthLog.jsx
+++ client/src/components/MyHealthLog.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { Grid } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import WeeklyTracker from './WeeklyTracker';
 import WeeklyTrackerDay from './WeeklyTrackerDay';
 import AppCalendar from './Calendar';
-import {makeStyles} from '@material-ui/core'
+
 const useStyles = makeStyles({
   button: {
     background: `linear-gradient(45deg, #4760ff, #82a4f8)`,
     width: '320px',
     boxShadow: '0px 1px 15px 0px #4760ff',
     margin: '10px',
-    color: 'white'
-  }
-})
+    color: 'white',
+  },
+});
 
 const dates = [
   {
@@ -30,7 +30,7 @@ const dates = [
 
 function HealthLogButton() {
   const { t } = useTranslation();
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div>
       <Link to="/symptomsurvey">
