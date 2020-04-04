3dd703f66d11e8d93d24285f914a46dfe7148296
Carter Klein <carterklein13@gmail.com>
2020-04-03 23:13:04 -0400

    Show detail on slide

--- client/src/components/HealthLogToggle.jsx
+++ client/src/components/HealthLogToggle.jsx
@@ -1,23 +1,57 @@
-import React, { useState } from 'react';
+/* eslint-disable no-await-in-loop */
+
+import React from 'react';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { Trans } from 'react-i18next';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { useBlockstack } from 'react-blockstack';
 import MyHealthLog from './MyHealthLog';
 import Table from './Table';
+import actions from '../redux/actions/actions';
 
-export default function HealthLogToggle() {
-  const [toggleValue, setToggleValue] = useState('myHealthLog');
-  const setHealthLog = () => setToggleValue('myHealthLog');
-  const setShowMeMore = () => setToggleValue('showMeMore');
+const HealthLogToggle = props => {
+  const { setDetailData } = props;
+  const data = [];
+  const { userSession } = useBlockstack();
+  let files = [];
+
+  const getDetailData = async () => {
+    files = [];
+    await userSession
+      .listFiles(file => {
+        files.push(file);
+        return true;
+      })
+      .then(async () => {
+        for (let i = 0; i < files.length; i += 1) {
+          if (files[i].includes('observation/')) {
+            const curr = await userSession.getFile(files[i]);
+            data.push(JSON.parse(curr));
+          }
+        }
+      });
+  };
+
+  const { toggleValue, setToggleValue } = props;
   return (
     <div>
       <Container>
         <ButtonGroup size="medium" color="secondary" aria-label="outlined button group" style={{ padding: '10px' }}>
-          <Button onClick={setHealthLog} variant={toggleValue === 'myHealthLog' && 'contained'}>
+          <Button onClick={() => setToggleValue('showMeMore')} variant={toggleValue === 'myHealthLog' && 'contained'}>
             <Trans i18nKey="health.logButton" />
           </Button>
-          <Button onClick={setShowMeMore} variant={toggleValue === 'showMeMore' && 'contained'}>
+          <Button
+            onClick={() => {
+              getDetailData().then(() => {
+                setDetailData(data);
+                setToggleValue('myHealthLog');
+              });
+            }}
+            variant={toggleValue === 'showMeMore' && 'contained'}
+          >
             <Trans i18nKey="health.showMoreButton" />
           </Button>
         </ButtonGroup>
@@ -26,4 +60,25 @@ export default function HealthLogToggle() {
       {toggleValue === 'showMeMore' && <Table />}
     </div>
   );
-}
+};
+
+HealthLogToggle.propTypes = {
+  toggleValue: PropTypes.string.isRequired,
+  setToggleValue: PropTypes.func.isRequired,
+  setDetailData: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => {
+  return {
+    toggleValue: state.healthToggleReducer.toggleValue,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setToggleValue: toggleValue => dispatch(actions.setToggleValue(toggleValue)),
+    setDetailData: detailData => dispatch(actions.setDetailData(detailData)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HealthLogToggle);
--- client/src/tests/WeeklyTrackerTest.test.jsx
+++ client/src/tests/WeeklyTrackerTest.test.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import WeeklyTracker from '../components/WeeklyTracker';
+
+const mockStore = configureMockStore();
 
 describe('WeeklyTracker', () => {
   it('should render a WeeklyTracker component ', () => {
+    const store = mockStore();
     window.HTMLCanvasElement.prototype.getContext = () => {};
 
-    const wrapper = mount(<WeeklyTracker />);
+    const wrapper = mount(
+      <Provider store={store}>
+        <WeeklyTracker chartType="test" />
+      </Provider>
+    );
     expect(wrapper.find('WeeklyTracker')).toMatchSnapshot();
   });
 });
