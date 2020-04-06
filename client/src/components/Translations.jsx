891b7097b2c11bcf174c2b5d3e2e3ae83c555401
Afrogrrrl <53954135+Afrogrrrl@users.noreply.github.com>
2020-04-06 16:16:03 -0400

    Lang selector (#414)

--- client/src/components/Translations.jsx
+++ client/src/components/Translations.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { useTranslation } from 'react-i18next';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 export default function TranslationsMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState('');
+
+  const classes = useStyles();
 
   const handleClick = event => {
-    setAnchorEl(event.currentTarget);
+    setAnchorEl(event.target.value);
+    changeLanguage(event.target.value);
   };
 
   const handleClose = () => {
-    setAnchorEl(null);
+    setAnchorEl('');
   };
   const { i18n, t } = useTranslation();
 
@@ -22,16 +37,23 @@ export default function TranslationsMenu() {
 
   return (
     <div>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        {t('selectLan')}
-      </Button>
-      <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-        <MenuItem onClick={() => changeLanguage('en')}>English</MenuItem>
-        <MenuItem onClick={() => changeLanguage('es')}>Spanish</MenuItem>
-        <MenuItem onClick={() => changeLanguage('it')}>Italian</MenuItem>
-        <MenuItem onClick={() => changeLanguage('fr')}>French</MenuItem>
-        <MenuItem onClick={() => changeLanguage('rus')}>Russian</MenuItem>
-      </Menu>
+      <FormControl className={classes.formControl}>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          keepMounted
+          onClose={handleClose}
+          value={anchorEl}
+          onChange={handleClick}
+        >
+          <MenuItem value={'en'}>English</MenuItem>
+          <MenuItem value={'es'}>Spanish</MenuItem>
+          <MenuItem value={'it'}>Italian</MenuItem>
+          <MenuItem value={'fr'}>French</MenuItem>
+          <MenuItem value={'rus'}>Russian</MenuItem>
+        </Select>
+        <FormHelperText>{t('selectLan')}</FormHelperText>
+      </FormControl>
     </div>
   );
 }
