194f00f71ce9f6871c3b0bde926346fecf1625f1
Tyler Deans <twdeans@gmail.com>
2020-04-07 22:33:29 -0400

    create custom hook for numObservations (#430)

---
+++ client/src/hooks/num-observations.js
@@ -0,0 +1,6 @@
+import { useSelector } from 'react-redux';
+
+export function useNumObservations() {
+  const numObservations = useSelector(state => state.observationsReducer.numObservations);
+  return numObservations;
+}
