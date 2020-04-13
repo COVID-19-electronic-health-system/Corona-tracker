c5f5d044c69c640f347f27bc5a5611480d51c352
pavel ilin <refugeebase@gmail.com>
2020-04-13 11:19:43 -0400

    ShareButtons component basic test

--- client/src/components/ShareButtons.jsx
+++ client/src/components/ShareButtons.jsx
@@ -1,41 +1,39 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import {
-    FacebookShareButton,
-    LinkedinShareButton,
-    RedditShareButton,
-    TelegramShareButton,
-    TwitterShareButton,
-    FacebookIcon,
-    LinkedinIcon,
-    TelegramIcon, 
-    TwitterIcon,
-    RedditIcon,
-  } from "react-share";
-
-
+  FacebookShareButton,
+  LinkedinShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  LinkedinIcon,
+  TelegramIcon,
+  TwitterIcon,
+  RedditIcon,
+} from 'react-share';
 
 const ShareButtons = () => {
-    const url = 'https://coronatracker.squarespace.com'
+  const url = 'https://coronatracker.squarespace.com';
 
-    return (
-        <Fragment>
-            <FacebookShareButton url={url}>
-                <FacebookIcon size={32} round={true} />
-            </FacebookShareButton>
-            <LinkedinShareButton url={url}>
-                <LinkedinIcon size={32} round={true} />
-            </LinkedinShareButton>
-            <TwitterShareButton url={url}>
-                <TwitterIcon size={32} round={true} />
-            </TwitterShareButton>
-            <RedditShareButton url={url}>
-                <RedditIcon size={32} round={true} />
-            </RedditShareButton>
-            <TelegramShareButton url={url}>
-                <TelegramIcon size={32} round={true} />
-            </TelegramShareButton>
-        </Fragment>
-    )
-}
+  return (
+    <>
+      <FacebookShareButton url={url}>
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+      <LinkedinShareButton url={url}>
+        <LinkedinIcon size={32} round />
+      </LinkedinShareButton>
+      <TwitterShareButton url={url}>
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+      <RedditShareButton url={url}>
+        <RedditIcon size={32} round />
+      </RedditShareButton>
+      <TelegramShareButton url={url}>
+        <TelegramIcon size={32} round />
+      </TelegramShareButton>
+    </>
+  );
+};
 
-export default ShareButtons
+export default ShareButtons;
