d5e9d3dd8d1a3a0a6a426c48c69a00863d2e23fd
JWaltuch <jenna.waltuch@gmail.com>
2020-04-15 11:18:49 -0400

    resolving merge conflicts

--- client/src/components/OnboardUser.jsx
+++ client/src/components/OnboardUser.jsx
@@ -8,11 +8,9 @@ import { useHistory } from 'react-router-dom';
 import { useBlockstack } from 'react-blockstack';
 import { Typography, Select, MenuItem, FormControl, Grid, ButtonGroup, Button } from '@material-ui/core';
 import actions from '../redux/actions/actions';
-// import { profileImg } from '../utils/imgUrl';
-let profileImg = 'temp';
 import DeletionDialog from './DeletionDialog';
-
 import buttonsCss from '../css/buttons';
+import { profileImg } from '../utils/imgUrl';
 
 const useStyles = makeStyles(() => ({
   root: {
