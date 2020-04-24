c0b9c203dfb33eb304557e84197a6da65b7eea7b
Adham Abo Hasson <55054963+AdhamAH@users.noreply.github.com>
2020-04-24 22:04:18 +0200

    update

--- client/src/components/showMeMore/ShowMeContainer.jsx
+++ client/src/components/showMeMore/ShowMeContainer.jsx
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
-import Observations from './Observations';
+import Observations from './Observation';
 import AvgTemperature from '../AvgTemperature';
 import Chart from '../Chart';
 import chartType from '../../utils/chartType';
