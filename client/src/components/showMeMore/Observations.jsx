0bd74a5d2ea426b295b49badd2194e8f1d1880ec
Adham Abo Hasson <55054963+AdhamAH@users.noreply.github.com>
2020-04-24 23:03:17 +0200

    Update Observations.jsx

--- client/src/components/showMeMore/Observations.jsx
+++ client/src/components/showMeMore/Observations.jsx
@@ -45,7 +45,7 @@ const WhiteFont = styled(Typography)({
 const Observations = props => {
   const classes = useStyles();
 
-  const { observations } = props;
+  const {detailData, observations } = props;
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(4);
@@ -60,96 +60,98 @@ const Observations = props => {
   };
   return (
     <div className={classes.root}>
-      {observations.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(observation => (
-        <ExpansionPanel className={classes.expansionPanel}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="observation"
-            id="observation"
-            className={classes.summary}
-          >
-            <Grid container alignItems="center" justify="center" spacing={1}>
-              <Grid item justify="center">
-                <RedFont>
-                  <span>Date:</span> {new Date(observation.date).toLocaleDateString()}
-                </RedFont>
+      {(detailData.length ? detailData : observations)
+        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+        .map(observation => (
+          <ExpansionPanel className={classes.expansionPanel}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="observation"
+              id="observation"
+              className={classes.summary}
+            >
+              <Grid container alignItems="center" justify="center" spacing={1}>
+                <Grid item justify="center">
+                  <RedFont>
+                    <span>Date:</span> {new Date(observation.date).toLocaleDateString()}
+                  </RedFont>
+                </Grid>
+                <Grid item>
+                  <RedFont>
+                    <span>Overall Feeling:</span> {`${observation.physical.dailyfeeling} /5`}
+                  </RedFont>
+                </Grid>
+                <Grid item>
+                  <RedFont>
+                    <span>Temperature:</span> {observation.physical.feverSeverity} &#8457;
+                  </RedFont>
+                </Grid>
               </Grid>
-              <Grid item>
-                <RedFont>
-                  <span>Overall Feeling:</span> {`${observation.physical.dailyfeeling} /5`}
-                </RedFont>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails className={classes.detailed}>
+              <Grid container alignItems="center" justify="center" spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Cough:</span> {observation.physical.coughSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Chills:</span> {observation.physical.chillsSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Shortness Of Breath:</span> {observation.physical.shortnessOfBreathSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Sore Throat:</span> {observation.physical.soreThroatSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Fatigue:</span> {observation.physical.fatigueSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Bluishness:</span> {observation.physical.bluishnessSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Gastrointestinnal Issues: </span>
+                    {observation.physical.giIssueSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Headache: </span>
+                    {observation.physical.headacheSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Loss of Smell:</span> {observation.physical.lostSmellSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <WhiteFont>
+                    <span>Loss of Taste:</span> {observation.physical.lostTasteSeverity}
+                  </WhiteFont>
+                </Grid>
+                <Grid item xs={12}>
+                  <WhiteFont>
+                    <span>Comments:</span>{' '}
+                    {observation.nonPhysical.openComment ? observation.nonPhysical.openComment : 'N/A'}
+                  </WhiteFont>
+                </Grid>
               </Grid>
-              <Grid item>
-                <RedFont>
-                  <span>Temperature:</span> {observation.physical.feverSeverity} &#8457;
-                </RedFont>
-              </Grid>
-            </Grid>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails className={classes.detailed}>
-            <Grid container alignItems="center" justify="center" spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Cough:</span> {observation.physical.coughSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Chills:</span> {observation.physical.chillsSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Shortness Of Breath:</span> {observation.physical.shortnessOfBreathSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Sore Throat:</span> {observation.physical.soreThroatSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Fatigue:</span> {observation.physical.fatigueSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Bluishness:</span> {observation.physical.bluishnessSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Gastrointestinnal Issues: </span>
-                  {observation.physical.giIssueSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Headache: </span>
-                  {observation.physical.headacheSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Loss of Smell:</span> {observation.physical.lostSmellSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <WhiteFont>
-                  <span>Loss of Taste:</span> {observation.physical.lostTasteSeverity}
-                </WhiteFont>
-              </Grid>
-              <Grid item xs={12}>
-                <WhiteFont>
-                  <span>Comments:</span>{' '}
-                  {observation.nonPhysical.openComment ? observation.nonPhysical.openComment : 'N/A'}
-                </WhiteFont>
-              </Grid>
-            </Grid>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      ))}
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        ))}
       <Grid container justify="center">
         <TablePagination
           rowsPerPageOptions={[2, 4, 8, 12]}
@@ -166,11 +168,14 @@ const Observations = props => {
 };
 
 Observations.propTypes = {
+  detailData: PropTypes.arrayOf(Object).isRequired,	
   observations: PropTypes.arrayOf(Object).isRequired,
 };
 
 const mapStateToProps = state => {
   return {
+    	
+    detailData: state.healthToggleReducer.detailData,	
     observations: state.observationsReducer.observations,
   };
 };
