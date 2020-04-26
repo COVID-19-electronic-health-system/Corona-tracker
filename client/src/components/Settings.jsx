f607b04cb1265db83a5585d0f7ca27661b77a066
petesamper <petesamper@gmail.com>
2020-04-25 21:37:22 -0400

    Changed update info button from Link to usehistory. Links for terms and policies.

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import TCLink from '@material-ui/core/Link';
+import { useHistory } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import PropTypes from 'prop-types';
 import { Typography, Grid, Button } from '@material-ui/core';
 import DeleteAllDataDialog from './DeleteAllDataDialog';
@@ -49,6 +49,7 @@ const Settings = props => {
   const { age, gender, city, state, zip, isSmoker, isObese, isAsthmatic } = demographicsComorbidities;
   const classes = useStyles();
   const [showDeletionDialog, setShowDeletionDialog] = useState(false);
+  const history = useHistory();
   return (
     <div>
       <Grid container justify="center" className={classes.root}>
@@ -155,7 +156,7 @@ const Settings = props => {
                 </Typography>
               </Grid>
             </Grid>
-            <Button className={classes.updateButton} component={Link} to="/onboard">
+            <Button className={classes.updateButton} onClick={() => history.push('/onboard')}>
               Update Information
             </Button>
           </Grid>
@@ -182,12 +183,12 @@ const Settings = props => {
         </Grid>
         <TranslationsMenu />
         <Grid container alignItems="center" direction="column">
-          <TCLink href="https://coronatracker.me/privacy-policy" color="textPrimary">
+          <Link href="https://coronatracker.me/privacy-policy" color="textPrimary">
             Privacy Policy
-          </TCLink>
-          <TCLink href="https://coronatracker.me/terms-of-use" color="textPrimary">
+          </Link>
+          <Link href="https://coronatracker.me/terms-of-use" color="textPrimary">
             Terms of Use
-          </TCLink>
+          </Link>
         </Grid>
       </Grid>
     </div>
