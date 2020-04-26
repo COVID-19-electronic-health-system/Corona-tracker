d39d2493e485aed81776f64b56fb5fa4f3316fd7
github username <Lukabaramishvili@gmail.com>
2020-04-26 15:53:21 -0400

    rename fun fact with just fact

--- client/src/utils/fakeCardData.js
+++ client/src/utils/fakeCardData.js
@@ -4,80 +4,80 @@ export const cardData = [
     body:
       'This virus was first detected in Wuhan City, Hubei Province, China. The first infections were linked to a live animal market, but the virus is now spreading from person-to-person.',
     img: 'https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg',
-    footer: 'fun fact# 1',
+    footer: 'fact# 1',
   },
   {
     title: 'Did you know?',
     body:
       'The virus causing coronavirus disease 2019 (COVID-19), is not the same as the coronaviruses that commonly circulate among humans and cause mild illness, like the common cold.',
     img: 'https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg',
-    footer: 'fun fact# 2',
+    footer: 'fact# 2',
   },
   {
     title: 'Did you know?',
     body:
       "In Covid-19,'CO' stands for 'corona', 'VI' for 'virus', and 'D' for disease. Formerly, this disease was referred to as 2019 novel coronavirus or 2019-nCov. There are many types of human coronaviruses including some that commonly cause mild upper-respiratory tract illness",
-    footer: 'fun fact# 3',
+    footer: 'fact# 3',
   },
   {
     title: 'Did you know?',
     body:
       'While SARS-CoV-2 originated in a wild animal market in Wuhan, China, the risk of catching a new coronavirus from animals remains very low. However, care should always be taken when handling raw meat, milk, or animal organs. Always ensure foods are properly cooked before consuming. (WHO)',
-    footer: 'fun fact# 4',
+    footer: 'fact# 4',
   },
   {
     title: 'Did you know?',
     body:
       'There is only one documented case of a dog being infected with SARS-CoV-2, so there is little evidence that pets can spread the disease. However, you should still wash your hands after handling pets, especially after walking your dog. Dogs could still contact contaminated surfaces and carry the virus externally. (WHO)',
-    footer: 'fun fact# 5',
+    footer: 'fact# 5',
   },
   {
     title: 'Did you know?',
     body:
       'It is safe to receive packages from areas where COVID-19 has been reported. The likelihood of an infected person contaminating commercial goods is low and the risk of catching the virus that causes COVID-19 from a package that has been moved, travelled, and exposed to different conditions and temperature is also low. (WHO)',
-    footer: 'fun fact# 6',
+    footer: 'fact# 6',
   },
   {
     title: 'Did you know?',
     body:
       'The official name of the virus is SARS-CoV-2 (Severe Acute Respiratory Syndrome Coronavirus 2). The original SARS-CoV emerged in 2002 and was responsible for the SARS outbreak. While the death rate of SARS was higher than COVID-19, it infected and killed far fewer people. There have been no cases of SARS since 2003.',
-    footer: 'fun fact# 7',
+    footer: 'fact# 7',
   },
   {
     title: 'Did you know?',
     body:
       'In order to fight a virus, your body’s immune system must be able to recognize it and destroy it. Viruses cannot be “killed” with medication because they are not alive.',
-    footer: 'fun fact# 8',
+    footer: 'fact# 8',
   },
   {
     title: 'Did you know?',
     body:
       'Viruses are not living organisms. Unlike bacteria, viruses are unable to replicate and live on their own. They must infect a host to survive and reproduce.',
-    footer: 'fun fact# 9',
+    footer: 'fact# 9',
   },
   {
     title: 'Did you know?',
     body:
       'The coronavirus is roughly 120nm in diameter. For reference, you could fit about 2,500 virus particles end to end on a single grain of salt!',
-    footer: 'fun fact# 10',
+    footer: 'fact# 10',
   },
   {
     title: 'Did you know?',
     body:
       'Around 1 out of every 6 people who gets COVID-19 becomes seriously ill and develops difficulty breathing. Older people, and those with underlying medical problems like high blood pressure, heart problems or diabetes, are more likely to develop serious illness. People with fever, cough and difficulty breathing should seek medical attention. (WHO)',
-    footer: 'fun fact# 11',
+    footer: 'fact# 11',
   },
   {
     title: 'Did you know?',
     body:
       'Studies to date suggest that COVID-19 is mainly transmitted through contact with respiratory droplets rather than through the air.',
-    footer: 'fun fact# 12',
+    footer: 'fact# 12',
   },
   {
     title: 'Did you know?',
     body:
       'The incubation period for COVID-19 ranges from 1 to 14 days, meaning it can take this long to start showing symptoms after initial infection. Some studies have shown that you are the most contagious during the early stages of the disease. This is why social distancing is so important to prevent asymptomatic transmission.',
-    footer: 'fun fact# 13',
+    footer: 'fact# 13',
   },
 ];
 
