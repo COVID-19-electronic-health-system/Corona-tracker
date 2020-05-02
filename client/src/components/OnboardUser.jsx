7fd5d0de753a73498b751f53324e1ba382491b99
Adham Abo Hasson <55054963+AdhamAH@users.noreply.github.com>
2020-05-02 07:57:04 +0200

    Onboard small screens fix (#687)
    * Onboard small screens fix
    
    * Onboard small screens fix
    
    * Update package.json

--- client/src/components/OnboardUser.jsx
+++ client/src/components/OnboardUser.jsx
@@ -22,6 +22,9 @@ import { states } from '../utils/constants';
 import { initialState as onboardingInitialState } from '../redux/reducers/onboarding';
 
 const useStyles = makeStyles(() => ({
+  root: {
+    paddingBottom: '100px',
+  },
   buttonRight: {
     ...buttonsCss.buttons,
     background: 'rgba(255,255,255,0.5)',
@@ -47,7 +50,7 @@ const useStyles = makeStyles(() => ({
     height: '3.5em',
     margin: '2em 8px 2px 8px',
     textTransform: 'uppercase',
-    width: '300px',
+    width: '275px',
   },
   fullWidth: {
     width: '100%',
@@ -79,7 +82,7 @@ const OnboardUser = props => {
   };
 
   return (
-    <Container maxWidth="xs">
+    <Container maxWidth="xs" className={classes.root}>
       <Grid container spacing={1} alignItems="center">
         <Grid item xs={12}>
           <Typography variant="h5">
@@ -149,7 +152,7 @@ const OnboardUser = props => {
             </Select>
           </FormControl>
         </Grid>
-        <Grid item sm={4} xs={6}>
+        <Grid item sm={5} xs={6}>
           <TextField
             type="text"
             name="zip"
@@ -160,8 +163,8 @@ const OnboardUser = props => {
             onChange={handleInputChange}
           />
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="subtitle2" color="textSecondary">
+        <Grid item xs={5}>
+          <Typography variant="body1" color="textSecondary">
             <b>Do you smoke?</b>
           </Typography>
         </Grid>
@@ -187,8 +190,8 @@ const OnboardUser = props => {
             </button>
           </ButtonGroup>
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="subtitle2" color="textSecondary">
+        <Grid item xs={5}>
+          <Typography variant="body1" color="textSecondary">
             <b>Are you Obese (BMI)?</b>
           </Typography>
         </Grid>
@@ -214,8 +217,8 @@ const OnboardUser = props => {
             </button>
           </ButtonGroup>
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="subtitle2" color="textSecondary">
+        <Grid item xs={5}>
+          <Typography variant="body1" color="textSecondary">
             <b>Do you have asthma?</b>
           </Typography>
         </Grid>
