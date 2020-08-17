549ce875ac65ca3e13a90810fa11b9b9b543b082
Adham Abo Hasson <55054963+AdhamAH@users.noreply.github.com>
2020-08-17 09:08:39 +0200

    add i18nkeys to layout components (#730)
    * add i18nkeys to layout components
    
    * Update Disclaimer.jsx
    
    * add i18nkeys to layout components
    
    * Update Disclaimer.jsx
    
    * rebase
    
    Co-authored-by: Adham Abo Hasson <adham.ahasson@gmail.com>

--- client/src/components/layout/NavBar.jsx
+++ client/src/components/layout/NavBar.jsx
@@ -7,12 +7,13 @@ import { connect } from 'react-redux';
 import FeaturedPlayListOutlinedIcon from '@material-ui/icons/FeaturedPlayListOutlined';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import SettingsIcon from '@material-ui/icons/Settings';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import PropTypes from 'prop-types';
 import actions from 'redux/actions/actions';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import SettingsButton from './SettingsButton';
+import { useTranslation } from 'react-i18next';
 import More from './More';
+import SettingsButton from './SettingsButton';
 
 const useStyles = makeStyles(theme => ({
   // making the style of NavBar
@@ -47,6 +48,7 @@ const useStyles = makeStyles(theme => ({
 const NavBar = props => {
   const { setMoreToggle, setToggleValue, setSettingsToggle, moreToggle, settingsToggle } = props;
   const classes = useStyles();
+  const { t } = useTranslation();
   return (
     // added to buttons when:
     // use route to a page: component={Link} to="/foo"
@@ -57,13 +59,13 @@ const NavBar = props => {
           <Button component={Link} to="/" className={classes.buttons} onClick={() => setToggleValue('myHealthLog')}>
             <Grid container direction="column" alignItems="center">
               <FeaturedPlayListOutlinedIcon className={classes.icons} />
-              <Typography variant="caption">Log</Typography>
+              <Typography variant="caption">{t('bottomNavMenu.text.log.log')}</Typography>
             </Grid>
           </Button>
           <Button component={Link} to="/education" className={classes.buttons}>
             <Grid container direction="column" alignItems="center">
               <StarBorderIcon className={classes.icons} />
-              <Typography variant="caption">education</Typography>
+              <Typography variant="caption">{t('bottomNavMenu.text.education.education')}</Typography>
             </Grid>
           </Button>
           <Button onClick={() => setMoreToggle(!moreToggle)} className={classes.buttons}>
@@ -75,14 +77,14 @@ const NavBar = props => {
           <Button component={Link} to="/map" className={classes.buttons}>
             <Grid container direction="column" alignItems="center">
               <LocationOnOutlinedIcon className={classes.icons} />
-              <Typography variant="caption">map</Typography>
+              <Typography variant="caption">{t('bottomNavMenu.text.map.map')}</Typography>
             </Grid>
           </Button>
 
           <Button onClick={() => setSettingsToggle(!settingsToggle)} className={classes.buttons}>
             <Grid container direction="column" alignItems="center">
-              <SettingsIcon className={classes.icons} />
-              <Typography variant="caption">settings</Typography>
+              <MoreHorizIcon className={classes.icons} />
+              <Typography variant="caption">{t('bottomNavMenu.text.more.more')}</Typography>
             </Grid>
           </Button>
         </Toolbar>
